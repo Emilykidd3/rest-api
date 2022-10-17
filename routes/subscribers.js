f16e136305c81df1da33cc1b329f7ebedf104a6a
emilykidd3 <e.kidd61@yahoo.com>
2022-10-16 21:34:26 -0700

    added create, update, and delete routes

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -2,19 +2,28 @@ const express = require('express')
 const router = express.Router()
 
 // getting all 
-router.get('/', (req,res) => {
-
+router.get('/', (req, res) => {
+    res.send('hello world')
 })
 
 // getting one
-router.get('/:id', (req,res) => {
+router.get('/:id', (req, res) => {
     
 })
 
 // creating one
+router.post('/', (req, res) => {
+
+})
 
 // updating one
+router.patch('/:id', (req, res) => {
+
+})
 
 // deleting one
+router.delete('/:id', (req, res) => {
+
+})
 
 module.exports = router
